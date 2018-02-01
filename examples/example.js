f3f06b59ea4e37b82b35cec0adad252b4b0ee06a
Dinesh <flexdinesh@gmail.com>
2018-02-02 00:25:29 +0530

    Refactored example to use the new export name

--- examples/example.js
+++ examples/example.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-console */
-const is = require('../lib');
+const check = require('../lib');
 
 const exampleObj = {
   goodKey: 'I exist :)'
 };
 
-if (is(exampleObj.goodKey).isDefined) { // true
+if (check(exampleObj.goodKey).isDefined) { // true
   console.log('Yaay, goodKey exists in exampleObj');
 }
 // in the console - 'Yaay, goodKey exists in exampleObj'
 
-if (is(exampleObj.badKey).isDefined) { // false
+if (check(exampleObj.badKey).isDefined) { // false
   console.log('Yaay, badKey exists in exampleObj');
 } else {
   console.log('Sad, badKey doesn\'t exist in exampleObj');
@@ -18,7 +18,7 @@ if (is(exampleObj.badKey).isDefined) { // false
 // in the console - 'Sad, badKey doesn't exist in exampleObj'
 
 const nullObj = null;
-if (is(nullObj).isNull) { // true
+if (check(nullObj).isNull) { // true
   console.log('Yo, nullObj is null');
 }
 // in the console - 'Yo, nullObj is null'
