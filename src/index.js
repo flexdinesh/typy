5b135825c293f88d6d6d87af8326dab7d323220f
Dinesh <flexdinesh@gmail.com>
2018-02-02 00:15:35 +0530

    Various types added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 
-class Is {
+class TypeUtil {
   constructor() {
+    this.resetVal();
+  }
+
+  resetVal() {
     this.isUndefined = false;
     this.isDefined = false;
     this.isNull = false;
+    this.isNullOrUndefined = false;
+    this.isBoolean = false;
+    this.isTrue = false;
     this.isFalse = false;
+    this.isTruthy = false;
+    this.isFalsy = false;
+    this.isObject = false;
+    this.isEmptyObject = false;
+    this.isString = false;
+    this.isEmptyString = false;
+    this.isNumber = false;
+    this.isInt = false;
+    this.isPositiveInt = false;
+    this.isNegativeInt = false;
+    this.isArray = false;
+    this.isEmptyArray = false;
+    this.isFunction = false;
   }
 
   evalUndefined = (obj) => {
@@ -15,24 +35,84 @@ class Is {
       } else {
         this.isDefined = false;
         this.isUndefined = true;
+        this.isFalsy = true;
+        this.isNullOrUndefined = true;
       }
     } catch (e) {
       this.isUndefined = true;
       this.isDefined = false;
+      this.isFalsy = true;
+      this.isNullOrUndefined = true;
     }
   }
 
   evalNull = (obj) => {
-    if (obj === null && typeof obj === 'object') this.isNull = true;
-    else this.isNull = false;
+    if (obj === null && typeof obj === 'object') {
+      this.isNull = true;
+      this.isNullOrUndefined = true;
+    } else {
+      this.isNull = false;
+    }
   }
 
-  is = (obj) => {
+  evalTruthyOrFalsy = (obj) => {
+    if (obj) {
+      this.isTruthy = true;
+      this.isFalsy = false;
+    }
+    if (!obj) {
+      this.isFalsy = true;
+      this.isTruthy = false;
+    }
+  }
+
+  evalType = (obj) => {
+    if (typeof obj === 'object' && obj !== null) {
+      this.isObject = true;
+      if (Object.keys(obj).length === 0) this.isEmptyObject = true;
+    }
+    if (typeof obj === 'string') {
+      this.isString = true;
+      if (obj.length === 0) this.isEmptyString = true;
+    }
+    if (Number.isInteger(obj)) {
+      this.isNumber = true;
+      this.isInt = true;
+      if (obj > 0) this.isPositiveInt = true;
+      if (obj < 0) this.isNegativeInt = true;
+    }
+    if (typeof obj === typeof (true)) {
+      this.isBoolean = true;
+      if (obj === true) {
+        this.isTrue = true;
+        this.isFalse = false;
+      }
+      if (obj === false) {
+        this.isFalse = true;
+        this.isTrue = false;
+      }
+    }
+    if (Array.isArray(obj)) {
+      this.isArray = true;
+      if (obj.length === 0) this.isEmptyArray = true;
+    }
+
+    if (typeof obj === 'function') {
+      this.isFunction = true;
+    }
+  }
+
+  check = (obj) => {
+    this.resetVal();
     this.evalUndefined(obj);
-    this.evalNull(obj);
+    if (this.isDefined) {
+      this.evalNull(obj);
+      this.evalTruthyOrFalsy(obj);
+      this.evalType(obj);
+    }
     return this;
   }
 }
 
-const { is } = new Is();
-export default is;
+const { check } = new TypeUtil();
+export default check;
