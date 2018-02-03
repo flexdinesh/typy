5c6af9bcd479b8fb9f6b533099903caa39ec4620
Dinesh <flexdinesh@gmail.com>
2018-02-03 17:50:52 +0530

    Added new checks to examples

--- examples/example.js
+++ examples/example.js
@@ -5,20 +5,31 @@ const exampleObj = {
   goodKey: 'I exist :)'
 };
 
-if (t(exampleObj.goodKey).isDefined) { // true
-  console.log('Yaay, goodKey exists in exampleObj');
-}
-// in the console - 'Yaay, goodKey exists in exampleObj'
-
-if (t(exampleObj.badKey).isDefined) { // false
-  console.log('Yaay, badKey exists in exampleObj');
-} else {
-  console.log('Sad, badKey doesn\'t exist in exampleObj');
-}
-// in the console - 'Sad, badKey doesn't exist in exampleObj'
+console.log(`exampleObj is definied - ${t(exampleObj).isDefined}`); // true
+console.log(`exampleObj.goodKey is definied - ${t(exampleObj, 'goodKey').isDefined}`); // true
+console.log(`exampleObj.badKey is definied - ${t(exampleObj, 'badKey').isDefined}`); // false
+console.log(`exampleObj.badKey is undefinied - ${t(exampleObj, 'badKey').isUndefined}`); // false
 
 const nullObj = null;
-if (t(nullObj).isNull) { // true
-  console.log('Yo, nullObj is null');
-}
-// in the console - 'Yo, nullObj is null'
+console.log(`nullObj is null - ${t(nullObj).isNull}`); // true
+
+const str = 'hello';
+console.log(`str is String - ${t(str).isString}`); // true
+
+const num = 22;
+console.log(`num is Number - ${t(num).isNumber}`); // true
+
+let bool = true;
+console.log(`bool is Boolean - ${t(bool).isBoolean}`); // true
+bool = true;
+console.log(`bool is true - ${t(bool).isTrue}`); // true
+bool = false;
+console.log(`bool is false - ${t(bool).isFalse}`); // true
+
+const obj = {};
+console.log(`obj is an Object - ${t(obj).isObject}`); // true
+console.log(`obj is an empty Object - ${t(obj).isEmptyObject}`); // true
+
+const arr = [];
+console.log(`arr is an Array - ${t(arr).isArray}`); // true
+console.log(`arr is an empty Array - ${t(arr).isEmptyArray}`); // true
