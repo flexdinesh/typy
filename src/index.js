b60da3752fd93c9317319dcc4bc4cfb23ac368fc
Dinesh <flexdinesh@gmail.com>
2018-02-20 23:17:38 +0530

    Merge separate typy files into one index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,136 @@
-import Typy from './typy';
+/* eslint-disable import/prefer-default-export */
+export const getNestedObject = (obj, dotSeparatedKeys) => {
+  if (arguments.length > 1 && typeof dotSeparatedKeys !== 'string') return undefined;
+  if (typeof obj !== 'undefined' && typeof dotSeparatedKeys === 'string') {
+    const pathArr = dotSeparatedKeys.split('.');
+    pathArr.forEach((key, idx, arr) => {
+      if (typeof key === 'string' && key.includes('[')) {
+        try {
+          arr.splice(idx + 1, 0, Number(/\[([^)]+)\]/.exec(key)[1]));
+          arr[idx] = key.slice(0, -3); // eslint-disable-line no-param-reassign
+        } catch (e) {
+          // do nothing
+        }
+      }
+    });
+    // eslint-disable-next-line no-param-reassign, no-confusing-arrow
+    obj = pathArr.reduce((o, key) => (o && o[key] !== 'undefined' ? o[key] : undefined), obj);
+  }
+  return obj;
+};
+
+
+export class Typy {
+  t = (obj, nestedKeys) => {
+    this.input = obj;
+
+    if (nestedKeys) {
+      this.input = getNestedObject(this.input, nestedKeys);
+    }
+
+    return this;
+  };
+
+  get isDefined() {
+    if (typeof this.input !== 'undefined') return true;
+    return false;
+  }
+
+  get isUndefined() {
+    if (typeof this.input === 'undefined') return true;
+    return false;
+  }
+
+  get isNull() {
+    if (this.input === null && typeof this.input === 'object') return true;
+    return false;
+  }
+
+  get isNullOrUndefined() {
+    if (this.isNull || this.isUndefined) return true;
+    return false;
+  }
+
+  get isBoolean() {
+    if (typeof this.input === typeof true) return true;
+    return false;
+  }
+
+  get isTrue() {
+    if (this.input === true) return true;
+    return false;
+  }
+
+  get isFalse() {
+    if (this.input === false) return true;
+    return false;
+  }
+
+  get isTruthy() {
+    if (this.input) return true;
+    return false;
+  }
+
+  get isFalsy() {
+    if (!this.input) return true;
+    return false;
+  }
+
+  get isObject() {
+    if (
+      typeof this.input === 'object' &&
+      this.input === Object(this.input) &&
+      Object.prototype.toString.call(this.input) !== '[object Array]'
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  get isEmptyObject() {
+    if (this.isObject && Object.keys(this.input).length === 0) return true;
+    return false;
+  }
+
+  get isString() {
+    if (typeof this.input === 'string') return true;
+    return false;
+  }
+
+  get isEmptyString() {
+    if (this.isString && this.input.length === 0) return true;
+    return false;
+  }
+
+  get isNumber() {
+    if (Number.isFinite(this.input)) return true;
+    return false;
+  }
+
+  get isArray() {
+    if (Array.isArray(this.input)) return true;
+    return false;
+  }
+
+  get isEmptyArray() {
+    if (this.isArray && this.input.length === 0) return true;
+    return false;
+  }
+
+  get isFunction() {
+    if (typeof this.input === 'function') return true;
+    return false;
+  }
+
+  get safeObject() {
+    return this.input;
+  }
+
+  get safeString() {
+    if (this.isString) return this.input;
+    return '';
+  }
+}
 
 const t = (input, objectPath) => new Typy().t(input, objectPath);
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import Typy from '../src/typy';
+import { Typy, getNestedObject } from '../src';
 
 describe('Typy', () => {
   const t = (input, objectPath) => new Typy().t(input, objectPath);
@@ -228,5 +228,72 @@ describe('Typy', () => {
         return input;
       });
     });
+  });
+});
+
+describe('Nested Object/Keys Check', () => {
+  it('should return nested object if exists', () => {
+    const mockObj = { goodKey: 'hello' };
+    assert(getNestedObject(mockObj, 'goodKey') !== undefined);
+    const mockDeepObj = {
+      nestedKey: {
+        goodKey: 'hello'
+      }
+    };
+    assert(getNestedObject(mockDeepObj, 'nestedKey.goodKey') === mockDeepObj.nestedKey.goodKey);
+    assert(getNestedObject(mockDeepObj, 'nestedKey.goodKey') !== undefined);
+  });
+
+  it('should return undefined if nested object does not exist', () => {
+    const mockObj = { goodKey: 'hello' };
+    assert(getNestedObject(mockObj, 'goodKey.badkey') === undefined);
+    const mockDeepObj = {
+      nestedKey: {
+        goodKey: 'hello'
+      }
+    };
+    assert(getNestedObject(mockDeepObj, 'badKey.goodKey') === undefined);
+    assert(getNestedObject(mockDeepObj, 'goodKey.badKey') === undefined);
+    assert(getNestedObject(mockDeepObj, 'nestedKey.badKey') === undefined);
+  });
+
+  it('should return nested object when array element is in path', () => {
+    let mockObj = {
+      nestedArray: [
+        { goodKey: 'hello one' },
+        { goodKey: 'hello two' }
+      ]
+    };
+    assert(getNestedObject(mockObj, 'nestedArray[0].goodKey') === 'hello one');
+    assert(getNestedObject(mockObj, 'nestedArray[1].goodKey') === 'hello two');
+
+    mockObj = {
+      nestedArray: ['a', 'b']
+    };
+    assert(getNestedObject(mockObj, 'nestedArray[0]') === 'a');
+    assert(getNestedObject(mockObj, 'nestedArray[1]') === 'b');
+  });
+
+  it('should return undefined when array element in path does not exist', () => {
+    const mockObj = {
+      nestedArray: [
+        { goodKey: 'hello one' },
+        { goodKey: 'hello two' }
+      ]
+    };
+    assert(getNestedObject(mockObj, 'badArray[0].goodKey') === undefined);
+    assert(getNestedObject(mockObj, 'nestedArray[1].goodKey.badkey') === undefined);
+    assert(getNestedObject(mockObj, 'nestedArray[2].goodKey') === undefined);
+    assert(getNestedObject(mockObj, 'nestedArray.goodKey') === undefined);
+  });
+
+  it('monkey test nested object', () => {
+    const mockObj = { goodKey: 'hello' };
+    assert(getNestedObject(mockObj, 'goodKey,badkey') === undefined);
+    assert(getNestedObject(mockObj, null) === undefined);
+    assert(getNestedObject(mockObj, 1) === undefined);
+    assert(getNestedObject(mockObj, 0) === undefined);
+    assert(getNestedObject(mockObj, {}) === undefined);
+    assert(getNestedObject(undefined, 'goodKey,badkey') === undefined);
   });
 });
