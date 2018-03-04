4f5c4f9fe29b366d6ffa645c36c09e258e787181
Dinesh <flexdinesh@gmail.com>
2018-03-04 14:54:11 +0530

    Add test cases to cover 100% of the code

--- test/typy.js
+++ test/typy.js
@@ -68,6 +68,12 @@ describe('Typy', () => {
         goodKey: 'hooray'
       };
       assert(t(obj.badKey).isNullOrUndefined === true, 'NullOrUndefined check didn\'t work :(');
+
+      assert(t('hello').isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
+      assert(t(false).isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
+      assert(t(22).isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
+      assert(t([]).isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
+      assert(t({}).isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
     });
   });
 
@@ -101,6 +107,11 @@ describe('Typy', () => {
     it('should test if object is empty', () => {
       const obj = {};
       assert(t(obj).isEmptyObject === true, 'Empty Object check didn\'t work :(');
+
+      assert(t('hello').isEmptyObject === false, 'Empty Object check didn\'t work :(');
+      assert(t(false).isEmptyObject === false, 'Empty Object check didn\'t work :(');
+      assert(t(22).isEmptyObject === false, 'Empty Object check didn\'t work :(');
+      assert(t([]).isEmptyObject === false, 'Empty Object check didn\'t work :(');
     });
 
     it('should test if type is string', () => {
@@ -111,6 +122,12 @@ describe('Typy', () => {
     it('should test if string is empty string', () => {
       const obj = '';
       assert(t(obj).isEmptyString === true, 'EmptyString check didn\'t work :(');
+
+      assert(t('hello').isEmptyString === false, 'EmptyString check didn\'t work :(');
+      assert(t(false).isEmptyString === false, 'EmptyString check didn\'t work :(');
+      assert(t(22).isEmptyString === false, 'EmptyString check didn\'t work :(');
+      assert(t([]).isEmptyString === false, 'EmptyString check didn\'t work :(');
+      assert(t({}).isEmptyString === false, 'FEmptyStringalse check didn\'t work :(');
     });
 
     it('should test if type is Number', () => {
@@ -136,11 +153,23 @@ describe('Typy', () => {
     it('should test if object is true', () => {
       const obj = true;
       assert(t(obj).isTrue === true, 'True check didn\'t work :(');
+
+      assert(t('hello').isTrue === false, 'True check didn\'t work :(');
+      assert(t(false).isNullOrUndefined === false, 'NullOrUndefined check didn\'t work :(');
+      assert(t(22).isTrue === false, 'True check didn\'t work :(');
+      assert(t([]).isTrue === false, 'True check didn\'t work :(');
+      assert(t({}).isTrue === false, 'True check didn\'t work :(');
     });
 
     it('should test if object is false', () => {
       const obj = false;
-      assert(t(obj).isFalse === true, 'True check didn\'t work :(');
+      assert(t(obj).isFalse === true, 'False check didn\'t work :(');
+
+      assert(t('hello').isFalse === false, 'False check didn\'t work :(');
+      assert(t(true).isFalse === false, 'False check didn\'t work :(');
+      assert(t(22).isFalse === false, 'False check didn\'t work :(');
+      assert(t([]).isFalse === false, 'False check didn\'t work :(');
+      assert(t({}).isFalse === false, 'False check didn\'t work :(');
     });
 
     it('should test if type is Array', () => {
@@ -152,6 +181,11 @@ describe('Typy', () => {
     it('should test if Array is Empty Array', () => {
       const obj = [];
       assert(t(obj).isEmptyArray === true, 'Empty Array check didn\'t work :(');
+
+      assert(t('').isEmptyArray === false, 'Empty Array check didn\'t work :(');
+      assert(t(false).isEmptyArray === false, 'Empty Array check didn\'t work :(');
+      assert(t(22).isEmptyArray === false, 'Empty Array check didn\'t work :(');
+      assert(t({}).isEmptyArray === false, 'Empty Array check didn\'t work :(');
     });
 
     it('should test if type is Function', () => {
