25ce3588e4c6bb294e28565de1bdb102082ead2a
Dan Wolfe <dwolfe2@nd.edu>
2019-01-10 14:28:09 -0500

    Adding support for bracket syntax
    See ticket: https://github.com/flexdinesh/typy/issues/14

--- src/util.js
+++ src/util.js
@@ -1,25 +1,10 @@
 const getNestedObject = (obj, dotSeparatedKeys) => {
   if (arguments.length > 1 && typeof dotSeparatedKeys !== 'string') return undefined;
   if (typeof obj !== 'undefined' && typeof dotSeparatedKeys === 'string') {
-    const pathArr = dotSeparatedKeys.split('.');
-    pathArr.forEach((key, idx, arr) => {
-      if (typeof key === 'string' && key.includes('[')) {
-        try {
-          // extract the array index as string
-          const pos = /\[([^)]+)\]/.exec(key)[1];
-          // get the index string length (i.e. '21'.length === 2)
-          const posLen = pos.length;
-          arr.splice(idx + 1, 0, Number(pos));
+    // split on ".", "[", "]", "'", """ and filter out empty elements
+    const splitRegex = /[.\[\]'"]/g; // eslint-disable-line no-useless-escape
+    const pathArr = dotSeparatedKeys.split(splitRegex).filter(k => k !== '');
 
-          // keep the key (array name) without the index comprehension:
-          // (i.e. key without [] (string of length 2)
-          // and the length of the index (posLen))
-          arr[idx] = key.slice(0, (-2 - posLen)); // eslint-disable-line no-param-reassign
-        } catch (e) {
-          // do nothing
-        }
-      }
-    });
     // eslint-disable-next-line no-param-reassign, no-confusing-arrow
     obj = pathArr.reduce((o, key) => (o && o[key] !== 'undefined' ? o[key] : undefined), obj);
   }
